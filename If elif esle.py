210cab53c927da095c4a031cb015c67331c650b1
Anuragsharma1311 <124031761+Anuragsharma1311@users.noreply.github.com>
2023-02-03 16:47:02 +0530

    Add files via upload

---
+++ If elif esle.py
@@ -0,0 +1,17 @@
+a=int(input('Enter your %'))
+if 90<a<=100:
+    print(a,'A1 🎯')
+elif 80<a<=90:
+    print(a,'A2 ')
+elif 70 <a<= 80:
+    print(a, 'B1')
+elif 60<a<=70:
+    print(a, 'B2')
+elif 50<a<=60:
+    print(a, 'C1')
+elif 40<a<=50:
+    print(a, 'C2')
+elif 32<=a<=40:
+    print(a,'D')
+else:
+    print(a,'Fail 😢')
